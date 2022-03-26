9e243e3a087fe550be3ae29278bfa52830e18caa
Gustavo saibro <gustavosaibro@gmail.com>
2022-03-26 15:06:40 -0300

    Created the store logic and state

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,30 @@
 import { createStore } from 'vuex'
 
 const store = createStore({
-    state: {},
-    getters: {},
-    mutations: {},
+    state: {
+        sidebar: {
+            collapsed: false,
+            sidebarWidth: 180
+        }
+    },
+    getters: {
+        sidebarWidth (state) {
+            return state.sidebar.sidebarWidth
+        },
+        isCollapsed (state) {
+            return state.sidebar.collapsed
+        }
+    },
+    mutations: {
+        toggleSidebar (state) {
+            if (!state.sidebar.collapsed) {
+                state.sidebar.sidebarWidth = 40
+            } else {
+                state.sidebar.sidebarWidth = 180
+            }
+            state.sidebar.collapsed = !state.sidebar.collapsed
+        }
+    },
     actions: {}
 })
 
