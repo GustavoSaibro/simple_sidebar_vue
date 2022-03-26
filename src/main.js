43021a64e392fed6eca97d143d8a398de26c3059
Gustavo saibro <gustavosaibro@gmail.com>
2022-03-26 15:11:23 -0300

    Installed store, routes and fontawesome'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store/store'
+import routes from './routes/routes'
+import '@fortawesome/fontawesome-free/js/all'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .use(routes)
+    .mount('#app')
